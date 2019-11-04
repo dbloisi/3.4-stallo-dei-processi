975e2f6c3f56fff94452fbebeec2bf248ed12e46
dbloisi <bloisi@dis.uniroma1.it>
2019-11-04 20:59:02 +0100

    trylock

---
+++ mutex-trylock.c
@@ -0,0 +1,80 @@
+/*
+* Corso di Sistemi Operativi
+* A.A. 2019/20
+* Corso di Laurea in Scienze e Tecnologie Informatiche
+* Universita' della Basilicata
+* 
+* docente: Ing. Domenico Daniele Bloisi, PhD
+*
+* domenico.bloisi@unibas.it
+*
+* mutex-trylock.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+pthread_mutex_t first_mutex;
+pthread_mutex_t second_mutex;
+
+void *do_work_one(void *param)
+{
+  int done = 0;
+  
+  while(!done) {
+    pthread_mutex_lock(&first_mutex); 
+    if(pthread_mutex_trylock(&second_mutex)) {
+      printf("doing work one\n");
+      pthread_mutex_unlock(&second_mutex);
+      pthread_mutex_unlock(&first_mutex);
+      done = 1;
+    }
+    else
+      pthread_mutex_unlock(&first_mutex);
+  }
+  pthread_exit(0);
+} 
+
+void *do_work_two(void *param)
+{
+  int done = 0;
+  
+  while(!done) {
+    pthread_mutex_lock(&second_mutex); 
+    if(pthread_mutex_trylock(&first_mutex)) {
+      printf("doing work two\n");
+      pthread_mutex_unlock(&first_mutex);
+      pthread_mutex_unlock(&second_mutex);
+      done = 1;
+    }
+    else
+      pthread_mutex_unlock(&second_mutex);
+  }
+  pthread_exit(0);
+} 
+
+int main()
+{
+  pthread_t thread_one, thread_two;
+
+  pthread_mutex_init(&first_mutex, NULL);
+  pthread_mutex_init(&second_mutex, NULL);
+
+  if(pthread_create(&thread_one, NULL, do_work_one, NULL) < 0)
+  {
+      printf("errore creazione thread_one\n");
+      exit(1);
+  }
+  
+  if(pthread_create(&thread_two, NULL, do_work_two, NULL) < 0)
+  {
+      printf("errore creazione thread_two\n");
+      exit(1);
+  }  
+  
+  pthread_join (thread_one, NULL);
+  pthread_join (thread_two, NULL);
+  return 0;
+}
+
