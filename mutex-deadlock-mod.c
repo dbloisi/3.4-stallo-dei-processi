99e0aecb3ce59de27055a13a51a414b8d577d38d
dbloisi <bloisi@dis.uniroma1.it>
2020-11-07 13:03:29 +0100

    deadlock mod

---
+++ mutex-deadlock-mod.c
@@ -0,0 +1,71 @@
+/*
+* Corso di Sistemi Operativi
+* Corso di Laurea in Scienze e Tecnologie Informatiche
+* Universita' della Basilicata
+* 
+* docente: Domenico D. Bloisi
+* domenico.bloisi@unibas.it
+*
+* **This code is provided without any warranty about its
+* usability. It is for educational purposes and should
+* be regarded as such.**
+*
+* mutex-deadlock-mod.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+pthread_mutex_t first_mutex;
+pthread_mutex_t second_mutex;
+
+void *do_work_one(void *param)
+{
+  pthread_mutex_lock(&first_mutex);
+  printf("work_one: first_mutex acquired. Going to sleep...\n");
+  sleep(1);
+  pthread_mutex_lock(&second_mutex);
+  printf("doing work one\n");
+  pthread_mutex_unlock(&second_mutex);
+  pthread_mutex_unlock(&first_mutex);
+  pthread_exit(0);
+} 
+
+void *do_work_two(void *param)
+{  
+  pthread_mutex_lock(&second_mutex);
+  printf("work_two: second_mutex acquired. Going to sleep...\n");
+  sleep(1);
+  pthread_mutex_lock(&first_mutex); 
+  printf("doing work two\n");  
+  pthread_mutex_unlock(&first_mutex);
+  pthread_mutex_unlock(&second_mutex);
+  pthread_exit(0);
+} 
+
+int main()
+{
+  pthread_t thread_one, thread_two;
+
+  pthread_mutex_init(&first_mutex, NULL);
+  pthread_mutex_init(&second_mutex, NULL);
+
+  if(pthread_create(&thread_one, NULL, do_work_one, NULL) < 0)
+  {
+      printf("errore creazione thread_one\n");
+      exit(1);
+  }
+  
+  if(pthread_create(&thread_two, NULL, do_work_two, NULL) < 0)
+  {
+      printf("errore creazione thread_two\n");
+      exit(1);
+  }  
+  
+  pthread_join (thread_one, NULL);
+  pthread_join (thread_two, NULL);
+  return 0;
+}
+
